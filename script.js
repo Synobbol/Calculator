5147735b0d41ac19ca3dd8191601387205353d37
Synobbol <97944378+Synobbol@users.noreply.github.com>
2023-02-09 15:31:31 +0100

    Add basic math operators

---
+++ script.js
@@ -0,0 +1,25 @@
+
+ /*    add
+    subtract
+    multiply
+    divide */
+function add(nbr1, nbr2 = 0){
+    return nbr1 + nbr2;
+}
+
+function substract(nbr1, nbr2 = 0){
+    return nbr1  - nbr2;
+}
+
+function multiply(nbr1, nbr2 = 0){
+    return nbr1 * nbr2;
+}
+
+function divide(nbr1, nbr2 = 0){
+    return nbr1 / nbr2;
+}
+
+console.log(add(1,2));
+console.log(substract(1,2));
+console.log(multiply(1,2));
+console.log(divide(1,2));
