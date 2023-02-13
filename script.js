c83f492aa35db15081e440f1ef37b3695c37215c
Synobbol <97944378+Synobbol@users.noreply.github.com>
2023-02-13 18:23:21 +0100

    Add operations and clear ability

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
- /*    add
-    subtract
-    multiply
-    divide */
+const arrayInput = {
+    list:[],
+    nbr1: [],
+    op: "",
+    nbr2: [],
+};
+
 function add(nbr1, nbr2 = 0){
     return nbr1 + nbr2;
 }
@@ -20,6 +23,8 @@ function divide(nbr1, nbr2 = 0){
 }
 
 function operate(nbr1,op,nbr2){
+    nbr1 = Number(nbr1);
+    nbr2 = Number(nbr2);
     switch (op) {
         case "+":
             console.log(nbr1,op, nbr2);
@@ -39,17 +44,75 @@ function operate(nbr1,op,nbr2){
     }
 
 }
-console.log(operate(1,"/",2));
 
 const display = document.querySelector("#input");
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => {
-    button.addEventListener("click", addToDisplay);  
+    button.addEventListener("click", addToDisplay); 
+    
 });
 
-function addToDisplay(){
-    display.value = this.value;
-    console.log("display: "+ display.value);
-    console.log("bouton: "+ this.value);
+
+function addToDisplay() {
+    let inputs = arrayInput;
+    switch (this.value){
+        case "":
+            del(inputs);
+            display.value = inputs.list;
+            // console.log("bouton: "+ inputs.list); 
+            break;
+        case "=":
+            fillObject(inputs);
+
+            let firstNbr = inputs.nbr1.join("");
+            let secondNbr = inputs.nbr2.join("");
+            /* console.log("num1 = " + firstNbr);
+            console.log("num2 = " + secondNbr); */
+    
+            let calcul = operate(firstNbr, inputs.op, secondNbr);
+            console.log("resultat calcul= " + calcul);
+            display.value = calcul;
+            del(inputs);
+            break;
+        default:
+            inputs.list.push(this.value);
+            inputs = inputs.list.join("");
+            display.value = inputs;     
+            // console.log("bouton: "+ inputs); 
+    }
+
+
+}
+
+function fillObject(arrayInput){
+    for (let i = 0; i < arrayInput.list.length; i++) {
+        if(arrayInput.list[i] === "+" || arrayInput.list[i] === "-" 
+        || arrayInput.list[i] === "*" || arrayInput.list[i] === "/"){
+            //isole l'opérateur
+            arrayInput.op = arrayInput.list[i];
+            console.log("opérateur: " + arrayInput.op);
+
+            //isole le nombre 1
+            let cut1 = arrayInput.list.slice(0,i);
+            
+            arrayInput.nbr1 = cut1;
+            console.log("nbr1: " + arrayInput.nbr1, cut1);
+            
+            //isole le nombre 2
+            let cut2 = arrayInput.list.slice(i + 1);
+            
+            arrayInput.nbr2 = cut2;
+            console.log("nbr2: " + arrayInput.nbr2);
+
+            return true;
+        }
+
+    }
+    return false;
+}
+
+function del(arrayI){
+    let del = arrayI.list = [];
+    return del;
 }
